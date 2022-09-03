cf540cc7309c1005282d2313278d8a1b59e6aab0
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2022-09-03 21:25:21 +0100

    nodemon

--- packagejsonmod.js
+++ packagejsonmod.js
@@ -1,2 +1,14 @@
 //npm init to install
 
+//Nodemon
+
+//Watch our files and restart our app.
+
+//the script object in our package.json is used to set shorts for our node command
+//for example:
+// if "start": 'node app.js', instead of pressing node app.js everytime, we can just 
+//enter npm start
+
+
+//to uninstall a package
+// npm uninstall packagename
